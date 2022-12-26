0fad0b82d0abb8fecd7f583e181f23f351e1cb1e
Ramir Azizov <azizoram@fel.cvut.cz>
2022-12-26 15:01:45 +0100

    initial commit base classes

---
+++ cmake-build-debug/CMakeFiles/semwork.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/ramir/studium/PCC/arkanoid/Ball.cpp" "/mnt/c/Users/ramir/studium/PCC/arkanoid/cmake-build-debug/CMakeFiles/semwork.dir/Ball.cpp.o"
+  "/mnt/c/Users/ramir/studium/PCC/arkanoid/Block.cpp" "/mnt/c/Users/ramir/studium/PCC/arkanoid/cmake-build-debug/CMakeFiles/semwork.dir/Block.cpp.o"
+  "/mnt/c/Users/ramir/studium/PCC/arkanoid/GayObject.cpp" "/mnt/c/Users/ramir/studium/PCC/arkanoid/cmake-build-debug/CMakeFiles/semwork.dir/GayObject.cpp.o"
+  "/mnt/c/Users/ramir/studium/PCC/arkanoid/Point.cpp" "/mnt/c/Users/ramir/studium/PCC/arkanoid/cmake-build-debug/CMakeFiles/semwork.dir/Point.cpp.o"
+  "/mnt/c/Users/ramir/studium/PCC/arkanoid/main.cpp" "/mnt/c/Users/ramir/studium/PCC/arkanoid/cmake-build-debug/CMakeFiles/semwork.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/semwork.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/semwork.dir/Ball.cpp.o"
+  "CMakeFiles/semwork.dir/Block.cpp.o"
+  "CMakeFiles/semwork.dir/GayObject.cpp.o"
+  "CMakeFiles/semwork.dir/Point.cpp.o"
+  "CMakeFiles/semwork.dir/main.cpp.o"
+  "semwork"
+  "semwork.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/semwork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
